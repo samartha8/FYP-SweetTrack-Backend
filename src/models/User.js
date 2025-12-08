1167c61e6d8cbae1d964ca1d6a8b7d4b9a9dbf08
samartha8 <shakyasamarth1@gmail.com>
2025-12-08 15:33:19 +0545

    feat: add User model with password hashing and authentication method

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  healthData: { type: mongoose.Schema.Types.ObjectId, ref: 'Health' },
+}, { timestamps: true });
+
+// Encrypt password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
