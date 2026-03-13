813312bad0e93776be9d7b7a1be761b5573b5578
samartha8 <shakyasamarth1@gmail.com>
2026-03-13 13:36:07 +0545

    feat(meal-routes): add meal image analysis, food classes, and nutrition lookup endpoints

--- src/routes/mealRoutes.js
+++ src/routes/mealRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createMealLog, deleteMealLog, getMealLog, listMealLogs, mealImageUpload } from '../controllers/mealController.js';
+import { createMealLog, deleteMealLog, getMealLog, listMealLogs, mealImageUpload, analyzeMeal, getFoodClasses, getNutritionForClass } from '../controllers/mealController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -11,10 +11,14 @@ router
   .get(listMealLogs)
   .post(mealImageUpload.single('image'), createMealLog);
 
+router.post('/analyze', mealImageUpload.single('image'), analyzeMeal);
+
+router.get('/food-classes', getFoodClasses);
+router.get('/nutrition-lookup', getNutritionForClass);
+
 router
   .route('/:id')
   .get(getMealLog)
   .delete(deleteMealLog);
 
 export default router;
-
