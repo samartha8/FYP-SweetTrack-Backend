eb7b39a29c0aec2979d73f6b08a57838ac843a9d
samartha8 <shakyasamarth1@gmail.com>
2026-05-24 16:16:09 +0545

    Add test notification route

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { checkGoalProgress, getGoals, registerPushToken, upsertGoals, updateWaterIntake } from '../controllers/notificationController.js';
+import { checkGoalProgress, getGoals, registerPushToken, sendTestNotification, upsertGoals, updateWaterIntake } from '../controllers/notificationController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.put('/goals', upsertGoals);
 router.get('/progress', checkGoalProgress);
 router.put('/water', updateWaterIntake);
 router.post('/push-token', registerPushToken);
+router.post('/test', sendTestNotification);
 
 export default router;
 
