8d57da9d4e9457a6da4d3671cdeb298e288024d9
samartha8 <shakyasamarth1@gmail.com>
2026-05-24 16:16:10 +0545

    Add reward redemption route

--- src/routes/rewardsRoutes.js
+++ src/routes/rewardsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { awardPoints, syncRewardsAndStreak } from '../controllers/rewardsController.js';
+import { awardPoints, syncRewardsAndStreak, redeemReward } from '../controllers/rewardsController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.use(protect);
 
 router.post('/sync', syncRewardsAndStreak);
 router.post('/award', awardPoints);
+router.post('/redeem', redeemReward);
 
 export default router;
+
