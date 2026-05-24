1532c14eae0339124ddc16f9fa6892edaa175e14
samartha8 <shakyasamarth1@gmail.com>
2026-05-24 16:16:08 +0545

    Add medical report scan route

--- src/routes/healthRoutes.js
+++ src/routes/healthRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getHealthData, saveHealthData } from '../controllers/healthController.js';
+import { getHealthData, saveHealthData, scanMedicalReport } from '../controllers/healthController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(protect, getHealthData).post(protect, saveHealthData);
+router.post('/scan', protect, scanMedicalReport);
 
 export default router;
